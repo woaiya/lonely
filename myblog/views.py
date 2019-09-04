baa0ae495c22398b16c0ee023a975461b6af7aad
workrk_test <1319378608@qq.com>
2019-09-04 14:58:08 +0800

    新的前端模板，博客详情页上下篇处理

--- myblog/views.py
+++ myblog/views.py
@@ -19,4 +19,19 @@ def graphic(request, graphic_id):
     图文博客详情页
     """
     graphic_data = GraphicRecordModels.objects.get(graphic_id=graphic_id)
-    return render(request, 'myblog/info.html', {'posts': graphic_data})
+    graphic_all = GraphicRecordModels.objects.exclude(graphic_state=0)
+    if int(graphic_id) > 1:
+        # 上一篇 __lt 小于graphic_id  last()最后一个ID
+        last_data = graphic_all.filter(graphic_id__lt=graphic_id).last()
+    else:
+        last_data = None
+    if int(graphic_id) < graphic_all.count():
+        # 上一篇 __gt 大于graphic_id  first()第一个ID
+        next_data = graphic_all.filter(graphic_id__gt=graphic_id).first()
+    else:
+        next_data = None
+    return render(request, 'myblog/info.html', {
+        'posts': graphic_data,
+        'last': last_data,
+        'next': next_data
+    })
